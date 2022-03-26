5b5dcde4e17a6f40aec3008733b5b7313fc092b3
ViniciosLimaDev <Viniciosdevweb@hotmail.com>
2022-03-25 21:48:56 -0300

    refactored code

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(function() {
                 let cit = $('#cd').text();
 
 
-                $.getJSON(`https://customsearch.googleapis.com/customsearch/v1?cx=629a8112478ebedb2&num=2&q=imagens da bandeira da cidade de ${cit}&searchType=image&key=AIzaSyCXOR3QnviZaioPsD7fPfajG5uoBxy0i0c`, function(response) {
+                $.getJSON(`https://customsearch.googleapis.com/customsearch/v1?cx=629a8112478ebedb2&num=2&q=imagens da bandeira da cidade de ${cit}&searchType=image&key=`, function(response) {
                     if (response != null || response != undefined) {
                         console.log(response)
                         var data = response.items[1].link;
